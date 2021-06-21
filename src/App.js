e8cf87778e203d58e8d7cc0d3c15cd0a2dd5d755
virginiacc <vczosek@gmail.com>
2021-06-21 08:31:51 -0700

    Add other entities

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import { useState } from 'react';
+import { useMemo } from 'react';
 import states from "./states.json";
-import stateGrantees from "./state-grantees.json";
+import granteeStates from "./grantees-states.json";
+import granteeTribes from "./grantees-tribes.json";
+import granteeTerritories from "./grantees-territories.json";
+
 import SelectComponent from "./SelectComponent.js";
 import Results from "./Results.js";
 
+const data = [...granteeStates, ...granteeTribes, ...granteeTerritories];
+
+function filterByState( state, data ) {
+
+}
+
 function App() {
   const [ selectedState, setSelectedState ] = useState( '' );
+  const [ selectedTypeFilter, setTypeFilter ] = useState( '' );
+  const [ selectedType, setSelectedType ] = useState( '' );
+
   return (
     <div className="App">
-      <label htmlFor="state-select">Filter by state</label>
+      <label htmlFor="state-select">Filter by state or territory</label>
       <SelectComponent items={ states } 
                        selectedValue={ selectedState }
-                       defaultText="select a state"
+                       defaultText="select a state or territory"
                        id="state-select"
                        handler={ setSelectedState }/>
-      <Results selectedState={ selectedState } data={ stateGrantees }/>
+      <Results selectedState={ selectedState } data={ data }/>
     </div>
   );
 }
